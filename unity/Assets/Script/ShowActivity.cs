453ddaf7646df3afc8e1573e18fe196080e9b5d4
stackos <stackos@qq.com>
2017-09-25 01:54:03 +0800

    add unity script.

---
+++ unity/Assets/Script/ShowActivity.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class ShowActivity : MonoBehaviour {
+	private void OnGUI() {
+		if(GUILayout.Button("Show Activity", GUILayout.Width(200), GUILayout.Height(50))) {
+			Debug.LogError("Show Activity");
+		}
+	}
+}
