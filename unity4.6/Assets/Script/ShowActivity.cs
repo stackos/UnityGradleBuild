3e4e37cfd53c5e843f7de4fa2453f51fd699055f
stackos <stackos@qq.com>
2017-09-26 00:43:27 +0800

    add unity 4.6.0 project.

---
+++ unity4.6/Assets/Script/ShowActivity.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ShowActivity : MonoBehaviour {
+	private void OnGUI() {
+		if(GUILayout.Button("Show Activity", GUILayout.Width(400), GUILayout.Height(100))) {
+			Debug.LogError("showActivity");
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+			AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+			AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+			jo.Call("showActivity");
+#endif
+		}
+	}
+}
