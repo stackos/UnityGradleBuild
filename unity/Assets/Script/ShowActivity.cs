ada71bfe4b6f4fb12238fd9027c3be4040d8c634
stackos <stackos@qq.com>
2017-09-25 23:35:31 +0800

    start activity which class in aar lib from unity c# script.

--- unity/Assets/Script/ShowActivity.cs
+++ unity/Assets/Script/ShowActivity.cs
@@ -2,8 +2,14 @@
 
 public class ShowActivity : MonoBehaviour {
 	private void OnGUI() {
-		if(GUILayout.Button("Show Activity", GUILayout.Width(200), GUILayout.Height(50))) {
-			Debug.LogError("Show Activity");
+		if(GUILayout.Button("Show Activity", GUILayout.Width(400), GUILayout.Height(100))) {
+			Debug.LogError("showActivity");
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+			AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+			AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+			jo.Call("showActivity");
+#endif
 		}
 	}
 }
